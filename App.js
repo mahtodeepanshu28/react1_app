4fd06f0104abb8ee6d2bf79b769eeffe90ce638f
Deepanshu <90448300+mahtodeepanshu28@users.noreply.github.com>
2022-05-12 16:24:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+
+
+import './App.css';
+import Preview from './practice/Preview';
+
+function App() {
+  return (
+    <div className="App">
+      <Preview />
+    </div>
+  );
+}
+
+export default App;
